1b75d8587317d7230e2983aa0adb8691cfe4dff0
adof96 <161426998+adof96@users.noreply.github.com>
2026-06-09 20:04:03 -0600

    feature engineering started

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,3 +1,38 @@
 def agregar_tamano_familia(df):
-    df['FamilySize'] = df['SibSp'] + df['Parch']
+
+    df['FamilySize'] = (
+        df['SibSp']
+        + df['Parch']
+        + 1
+    )
+
+    return df
+
+def agregar_is_alone(df):
+
+    df['IsAlone'] = (df['FamilySize'] == 1).astype(int)
+
+    return df
+
+
+def agregar_title(df):
+
+    df = df.copy()
+
+    df['Title'] = (
+        df['Name']
+        .str.extract(r' ([A-Za-z]+)\.', expand=False)
+    )
+
+    titulos_comunes = [
+        'Mr',
+        'Mrs',
+        'Miss',
+        'Master'
+    ]
+
+    df['Title'] = df['Title'].apply(
+        lambda x: x if x in titulos_comunes else 'Rare'
+    )
+
     return df
