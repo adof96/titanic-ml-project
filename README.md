c2cffbdab1f395634f99ae020356bc8810f1c7fd
adof96 <161426998+adof96@users.noreply.github.com>
2026-07-28 23:37:32 -0600

    Configure project as an installable Python package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Titanic Machine Learning Engineering Project
+
+Proyecto desarrollado para aprender Machine Learning Engineering y buenas prácticas de Data Science.
--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -6,10 +6,10 @@ import streamlit as st
 
 from src.models.model_io import cargar_modelo
 from src.config import PRODUCTION_MODEL_PATH
+
 # ============================
 # Configuración
 # ============================
-
 
 st.set_page_config(
     page_title="Titanic Survival Predictor",
@@ -18,10 +18,24 @@ st.set_page_config(
 )
 
 # ============================
+# Recursos
+# ============================
+
+@st.cache_resource
+def cargar_modelo_streamlit():
+    """
+    Carga el modelo entrenado una única vez.
+    """
+    return cargar_modelo(PRODUCTION_MODEL_PATH)
+
+
+modelo = cargar_modelo_streamlit()
+
+# ============================
 # Encabezado
 # ============================
 
-st.title("Prueba")
+st.title("🚢 Titanic Survival Predictor")
 
 st.write(
     """
@@ -29,23 +43,5 @@ st.write(
     la probabilidad de supervivencia de un pasajero del Titanic.
     """
 )
-# ============================
-# Recursos
-# ============================
 
-@st.cache_resource
-def cargar_modelo_streamlit():
-    """
-    Carga el modelo entrenado una única vez y lo reutiliza
-    durante toda la ejecución de la aplicación.
-    """
-    return cargar_modelo(PRODUCTION_MODEL_PATH)
-
-
-# ============================
-# Interfaz
-# ============================
-
-# ============================
-# Resultados
-# ============================
+st.success("✅ Modelo cargado correctamente.")
