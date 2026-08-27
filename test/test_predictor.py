d6a771beca951a534bcaef3e79476dd97b4a819c
adof96 <161426998+adof96@users.noreply.github.com>
2026-08-26 18:27:59 -0600

    final_test_predictor

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -1,17 +1,36 @@
+import pytest
 from src.inference.predictor import predecir_pasajero
-
+from src.config import FEATURES
 class ModeloFalso:
 
+    def __init__(self, prediccion, probabilidades):
+        self.prediccion = prediccion
+        self.probabilidades = probabilidades
+        self.datos_recibidos = None
+
     def predict(self, X):
-        return [1]
+        self.datos_recibidos = X
+        return [self.prediccion]
 
     def predict_proba(self, X):
-        return [[0.25, 0.75]]
+        return [self.probabilidades]
 
-def test_predecir_pasajero_devuelve_resultado_correcto():
+@pytest.mark.parametrize(
+    "prediccion, probabilidades, prob_not_survival, prob_survival",
+    [
+        (1, [0.25, 0.75], 0.25, 0.75),
+        (0, [0.80, 0.20], 0.80, 0.20),
+    ]
+)
+def test_predecir_pasajero(
+    prediccion,
+    probabilidades,
+    prob_not_survival,
+    prob_survival
+):
 
     # Arrange
-    modelo = ModeloFalso()
+    modelo = ModeloFalso(prediccion, probabilidades)
 
     # Act
     resultado = predecir_pasajero(
@@ -26,6 +45,7 @@ def test_predecir_pasajero_devuelve_resultado_correcto():
     )
 
     # Assert
-    assert resultado["prediction"] == 1
-    assert resultado["prob_not_survival"] == 0.25
-    assert resultado["prob_survival"] == 0.75
+    assert resultado["prediction"] == prediccion
+    assert resultado["prob_not_survival"] == prob_not_survival
+    assert resultado["prob_survival"] == prob_survival
+    assert list(modelo.datos_recibidos.columns) == FEATURES
